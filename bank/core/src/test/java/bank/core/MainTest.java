be1d5597638e6b2c3b27e0f0878c65d1c8114271
Aimár A. Chirico <aimarac@stud.ntnu.no>
2024-09-16 11:12:18 +0200

    maven filstruktur laget

---
+++ bank/core/src/test/java/bank/core/MainTest.java
@@ -0,0 +1,13 @@
+package bank.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    
+    @Test
+    public void testAssertTrue() {
+        Assertions.assertTrue(true);
+    }
+
+}
