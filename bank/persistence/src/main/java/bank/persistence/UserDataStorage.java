cb008e4290a4c3951b3f15e40e4b922d8d216024
Sindre Ulsaker <sindrul@stud.ntnu.no>
2024-09-27 14:43:59 +0200

    feat(persistance): add UserDataStorageTest.java Added test for UserDataStorage class. Also updated UserDataStorage to set defaultindentation and correct formatting of code to reflect the checkstyle requirements.
    Co-authored by: Felix

--- bank/persistence/src/main/java/bank/persistence/UserDataStorage.java
+++ bank/persistence/src/main/java/bank/persistence/UserDataStorage.java
@@ -8,52 +8,82 @@ import com.fasterxml.jackson.core.type.TypeReference;
 
 import bank.core.User;
 
+/**
+ * Class responsible for managing user data storage and retrieval. It interacts with the
+ * BankPersistence class to read and write user data to a specified file.
+ */
 public class UserDataStorage {
-    File file;
-    BankPersistence bankPers = new BankPersistence();
-    List<User> users = new ArrayList<>();
+  File file;
+  BankPersistence bankPers = new BankPersistence();
+  List<User> users = new ArrayList<>();
 
-    public UserDataStorage(String path) {
-      file = new File(path);
+  /**
+   * Constructs a UserDataStorage instance with the specified file path.
+   *
+   * @param path the path to the user data file
+   */
+  public UserDataStorage(String path) {
+    file = new File(path);
+  }
+
+  /**
+   * Checks if the user data file exists.
+   *
+   * @return true if the user data file exists, false otherwise
+   */
+  public boolean userDataExists() {
+    return file.exists();
+  }
+
+  /**
+   * Fetches user data from the file if it exists and populates the users list. If the file does not
+   * exist or cannot be read, an empty list is initialized.
+   */
+  public void fetchUserData() {
+    if (userDataExists()) {
+      users = bankPers.readUserData(file, new TypeReference<List<User>>() {});
+    }
+    if (users == null) {
+      users = new ArrayList<>();
+    }
+  }
+
+  /**
+   * Writes a user to the data file if the user does not already exist. It first fetches existing user
+   * data and checks for duplicates before writing.
+   *
+   * @param user the user to be added
+   */
+  public void writeUserData(User user) {
+    boolean exists = false;
+    fetchUserData();
+    for (User i : users) {
+      if (i.ssnEquals(user)) {
+        exists = true;
+        break;
+      }
+    }
+    if (!exists) {
+      users.add(user);
+      bankPers.writeToFile(file, users);
     }
 
-    public boolean userDataExists() {
-        return file.exists();
-    }
-    
-    public void fetchUserData() {
-        if (userDataExists()) {
-            users = bankPers.readUserData(file, new TypeReference<List<User>>() {});
-        }
-        if (users == null) {
-            users = new ArrayList<>();
-        }
-    }
-   
-    public void writeUserData(User user) {
-        boolean exists = false;
-        fetchUserData();
-        for(User i : users) {
-            if(i.ssnEquals(user)) {
-                exists = true;
-                break;
-            }
-        }
-        if(!exists) {
-            users.add(user);
-            bankPers.writeToFile(file, users);
-        }
+  }
 
+  /**
+   * Retrieves a user by their social security number (SSN).
+   *
+   * @param ssn the social security number of the user to retrieve
+   * @return the User object if found, or null if not found
+   */
+  public User getUser(String ssn) {
+    fetchUserData();
+    for (User i : users) {
+      if (i.getSsn().equals(ssn)) {
+        return i;
+      }
     }
-   
-    public User getUser(String ssn) {
-        fetchUserData();
-        for(User i : users) {
-            if(i.getSsn().equals(ssn)) {
-                return i;
-            }
-        }
-        return null;
-    }
+    return null;
+  }
 
 }
