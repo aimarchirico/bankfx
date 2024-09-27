78ba5a1e52c5b46b8c3e44b39e8f58b0f9eb5a3a
Max Felix E Storteig <mfstorte@stud.ntnu.no>
2024-09-27 13:49:12 +0200

    test(core): add test for account
    Added tests which test basic functionality of account class
    
    Co-Authored by sindre

---
+++ bank/core/src/test/java/bank/core/AccountTest.java
@@ -0,0 +1,38 @@
+package bank.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+    Account account1;
+
+    @BeforeEach
+    public void setup() {
+        account1 = new Account(100.0, "test", "Sparekonto");
+    }
+
+    @Test
+    public void testConstructor() {
+        assertThrows(IllegalArgumentException.class, () -> new Account(-1.0, "MinKonto", "Sparekonto")); //Illegal balance
+        assertThrows(IllegalArgumentException.class, () -> new Account(10.0, "MinAndreKonto", "Lån")); //Illegal accounttype
+        assertThrows(IllegalArgumentException.class, () -> new Account(10.0, "-*¨^", "Sparekonto")); //Illegal name
+    }
+
+    @Test
+    public void testWithdrawDeposit() {
+        assertEquals(100.0, account1.getBalance());
+        account1.deposit(100.0);
+        assertEquals(200.0, account1.getBalance());
+        account1.withDraw(50.0);
+        assertEquals(150, account1.getBalance());
+        assertThrows(IllegalArgumentException.class, () -> account1.withDraw(200.0));
+    }
+
+    @Test
+    public void testNameChange() {
+        assertThrows(IllegalArgumentException.class, () -> account1.changeName(""));
+    }
+}
