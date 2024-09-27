cdb0a1be1523e3c06c65ad4594826f956593b772
Aimár A. Chirico <aimarac@stud.ntnu.no>
2024-09-27 14:03:40 +0200

    feat(persistence): add utils classes for persistence
    Added Utils and TestUtils for persistence module to save file paths.
    
    Co-authored by: Kasper

---
+++ bank/persistence/src/main/java/bank/persistence/Utils.java
@@ -0,0 +1,10 @@
+package bank.persistence;
+
+/**
+ * Class for utility methods used in the controllers.
+ */
+public class Utils {
+
+  public static final String path = "../persistence/src/main/resources/bank/persistence/UserData.json";
+
+}
---
+++ bank/persistence/src/test/java/bank/persistence/TestUtils.java
@@ -0,0 +1,10 @@
+package bank.persistence;
+
+/**
+ * Class for utility methods used in the controllers.
+ */
+public class TestUtils {
+
+  public static final String path = "../persistence/src/test/resources/bank/persistence/UserData.json";
+
+}
