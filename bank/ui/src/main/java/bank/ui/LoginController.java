e2e422fd868942ff5d3e406174ba1ac700884e93
Max Felix E Storteig <mfstorte@stud.ntnu.no>
2024-10-28 11:56:42 +0100

    feat(ui): use UserAccess in LoginController changed methods in LoginController to use UserAccess Co-Authored by: Sindre

--- bank/ui/src/main/java/bank/ui/LoginController.java
+++ bank/ui/src/main/java/bank/ui/LoginController.java
@@ -4,6 +4,7 @@ import bank.core.User;
 import bank.persistence.UserDataStorage;
 import bank.persistence.Utils;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
@@ -38,8 +39,7 @@ public class LoginController {
   }
 
   /**
-   * Dismiss error message.
-   * Delegates to UiUtils.
+   * Dismiss error message. Delegates to UiUtils.
    */
   @FXML
   private void dismissError() {
@@ -53,15 +53,20 @@ public class LoginController {
    */
   @FXML
   private void login() throws IOException {
-    UserDataStorage storage = new UserDataStorage(Utils.path);
-    User user = storage.getUser(ssnField.getText());
-    if (user == null || !user.getPassword().equals(passwordField.getText())) {
-      UiUtils.showError(errorButton, "Incorrect social security number or password");
+    try {
+      UserAccess userAccess = new UserAccess();
+      try {
+        userAccess.getUserRequest(ssnField.getText(), passwordField.getText());
+        FXMLLoader fxmlLoader = UiUtils.newScene(this, loginButton, "Overview.fxml");
+        OverviewController controller = fxmlLoader.getController();
+        User user = userAccess.getUser();
+        controller.setUser(user);
 
-    } else {
-      FXMLLoader fxmlLoader = UiUtils.newScene(this, loginButton, "Overview.fxml");
-      OverviewController controller = fxmlLoader.getController();
-      controller.setUser(user);
+      } catch (Exception e) {
+        UiUtils.showError(errorButton, e.getMessage());
+      }
+    } catch (URISyntaxException e) {
+      UiUtils.showError(errorButton, "Error contacting server");
     }
   }
 
