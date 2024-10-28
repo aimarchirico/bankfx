ea717973093db2e2aa8645f38b91d2761f95611b
Aimár A. Chirico <aimarac@stud.ntnu.no>
2024-10-28 12:20:44 +0100

    fix(parent): update pom files
    Updateed pom files for new dependencies.
    
    Co-authored by: Felix

---
+++ bank/rest/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module bank.rest {
+  requires bank.core;
+  requires bank.persistence;
+  requires com.fasterxml.jackson.databind;
+  requires spring.web;
+  requires spring.beans;
+  requires spring.boot;
+  requires spring.context;
+  requires spring.boot.autoconfigure;
+  requires spring.webmvc;
+
+  opens bank.rest to spring.beans, spring.context, spring.web;
+
+}
