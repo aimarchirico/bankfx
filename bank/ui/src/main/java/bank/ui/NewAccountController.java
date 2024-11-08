7892fc782b5533eb29248acb0c223ed4860079ee
Kasper Andreas Haugaard <kaspeaha@stud.ntnu.no>
2024-11-08 14:56:16 +0100

    feat(ui): expand newAccount to handle new classes Expanded the controllerclass, testclass and fxml-file of newAccount to handle more classes Co-authored by: Aimar

--- bank/ui/src/main/java/bank/ui/NewAccountController.java
+++ bank/ui/src/main/java/bank/ui/NewAccountController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
@@ -19,9 +20,17 @@ public class NewAccountController extends Controller {
   @FXML
   private Button confirmButton;
   @FXML
+  private ImageView paymentIcon;
+  @FXML
+  private ImageView transferIcon;
+  @FXML
+  private ImageView withdrawalIcon;
+  @FXML
+  private ImageView depositIcon;
+  @FXML
   private TextField nameField;
   @FXML
-  private TextField typeField;
+  private ChoiceBox<String> accountTypes;
   @FXML
   private Button errorButton;
 
@@ -49,18 +58,70 @@ public class NewAccountController extends Controller {
   }
 
   /**
-   * Create a new account.
+   * Open payment scene.
    *
    * @throws IOException when file is invalid
    */
   @FXML
-  private void newAccount() throws IOException {
+  private void openPayment() throws IOException {
+    FXMLLoader fxmlLoader = newScene(this, paymentIcon, "Payment.fxml");
+    PaymentController paymentController = fxmlLoader.getController();
+    paymentController.setUserAccess(userAccess);
+    paymentController.update();
+  }
+
+  /**
+   * Opens the {@link DepositController} scene for making a deposit.
+   *
+   * @throws IOException if the scene file is invalid
+   */
+  @FXML
+  private void openDeposit() throws IOException {
+    FXMLLoader fxmlLoader = newScene(this, depositIcon, "Deposit.fxml");
+    DepositController controller = fxmlLoader.getController();
+    controller.setUserAccess(userAccess);
+    controller.update();
+  }
+
+  /**
+   * Opens the {@link WithdrawalController} scene for making a withdrawal.
+   *
+   * @throws IOException if the scene file is invalid
+   */
+  @FXML
+  private void openWithdrawal() throws IOException {
+    FXMLLoader fxmlLoader = newScene(this, withdrawalIcon, "Withdrawal.fxml");
+    WithdrawalController controller = fxmlLoader.getController();
+    controller.setUserAccess(userAccess);
+    controller.update();
+  }
+
+
+  /**
+   * Opens the {@link TransferController} scene for initiating a transfer.
+   *
+   * @throws IOException if the scene file is invalid
+   */
+  @FXML
+  private void openTransfer() throws IOException {
+    FXMLLoader fxmlLoader = newScene(this, transferIcon, "Transfer.fxml");
+    TransferController controller = fxmlLoader.getController();
+    controller.setUserAccess(userAccess);
+    controller.update();
+  }
+
+  /**
+   * Handles the creation of an account based on user input,
+   * initiates the createAccount request.
+   */
+  @FXML
+  private void newAccount() {
     try {
-      userAccess.createAccountRequest(new Account(0.0, nameField.getText(), typeField.getText()));
+      userAccess.createAccountRequest(new Account(0.0, nameField.getText(), accountTypes.getValue()));
       openOverview(confirmButton);
     } catch (Exception e) {
       showError(e.getMessage());
     }
-
   }
+
 }
